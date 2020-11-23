b69b3e2ed730ddcf5becea74864e4cc398bed244
Valeria Heikkila <valeria.heikkila@gmail.com>
2020-11-23 18:12:19 +0900

    Initial Commit

--- AnalizeByTwitter/ViewController.swift
+++ AnalizeByTwitter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BackgroundView: UIView!
+    @IBOutlet weak var Icon: UILabel!
+    @IBOutlet weak var InputField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func PredictPressed(_ sender: UIButton) {
+    }
 }
 
